ecfbbb4e9defeef557e43d869b4b456a0287e963
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-27 18:50:31 +0100

    mise a defaut

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,8 @@
-import Image from "next/image";
 import styles from "./page.module.css";
-import { Button } from "@/components/ui/button";
-import { FaBeer } from "react-icons/fa";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <FaBeer></FaBeer>
     </div>
   )
 }
