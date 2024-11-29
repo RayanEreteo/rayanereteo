d027622df51036cca30deb7fdca66fae9f246e99
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-29 18:54:11 +0100

    ajout supplementaires

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
 import Navbar from "@/components/Navbar";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Button } from "@/components/ui/button";
+import { Box, Flex, Heading, HStack, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -9,11 +10,15 @@ export default function Home() {
       <Navbar></Navbar>
       <main>
         <section id="home" style={{marginTop: "200px", marginLeft: "60px"}}>
-          <Flex flexDir={"row"}>
+          <Flex flexDir={"column"}>
             <Box id="main-title">
               <Heading fontSize={80}>Rayan Ereteo</Heading>
-              <Text mt={"50px"} w={"800px"}>Je suis Rayan, un développeur full stack de 22 ans, passionné d'informatique depuis tout jeune, j'ai découvert la programmation avec le moteur de jeu Unity. Naturellement je me suis intéressé de plus en plus à la programmation.</Text>
+              <Text mt={"50px"} w={"800px"} fontStyle={"italic"}>Je suis Rayan, un développeur full stack de 22 ans, passionné d'informatique depuis tout jeune, j'ai découvert la programmation avec le moteur de jeu Unity. Naturellement je me suis intéressé de plus en plus à la programmation, ce qui m'a mené où je suis aujourd'hui. Spécialisé dans le développement full stack, je suis à la recherche de nouvelles missions et opportunités pour améliorer mes compétences.</Text>
             </Box>
+            <HStack mt={6} spaceX={"10"} ml={"6"}>
+              <Button backgroundColor={"blue"}>Me contacter</Button>
+              <Button backgroundColor={"white"} color={"black"}>Mes projets</Button>
+            </HStack>
           </Flex>
         </section>
       </main>
