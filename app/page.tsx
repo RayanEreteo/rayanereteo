05d0de3fddf73f7c7a3bf3219774467c481f583e
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-29 15:29:13 +0100

    use client et ajout W

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+"use client"
+
 import Navbar from "@/components/Navbar";
 import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
@@ -10,7 +12,7 @@ export default function Home() {
           <Flex flexDir={"row"}>
             <Box id="main-title">
               <Heading fontSize={80}>Rayan Ereteo</Heading>
-              <Text mt={"50px"}>Je suis Rayan, un développeur de 22 ans, </Text>
+              <Text mt={"50px"} w={"800px"}>Je suis Rayan, un développeur full stack de 22 ans, passionné d'informatique depuis tout jeune, j'ai découvert la programmation avec le moteur de jeu Unity. Naturellement je me suis intéressé de plus en plus à la programmation.</Text>
             </Box>
           </Flex>
         </section>
