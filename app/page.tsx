380c20201bbf2aaf010e6589f3fcb896bcc4f4e1
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-29 14:08:27 +0100

    ajout framer-motion et changement page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,20 @@
 import Navbar from "@/components/Navbar";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
     <Box>
       <Navbar></Navbar>
-      <main></main>
+      <main>
+        <section id="home" style={{marginTop: "200px", marginLeft: "60px"}}>
+          <Flex flexDir={"row"}>
+            <Box id="main-title">
+              <Heading fontSize={80}>Rayan Ereteo</Heading>
+              <Text mt={"50px"}>Je suis Rayan, un développeur de 22 ans, </Text>
+            </Box>
+          </Flex>
+        </section>
+      </main>
     </Box>
   )
 }
