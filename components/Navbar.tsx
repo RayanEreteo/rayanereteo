ec83710dc946cdb2f828f1e226daa879502176ee
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-11-29 15:28:58 +0100

    Changement nom lien

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
       <nav>
         <HStack backgroundColor={"rgba(0, 0, 0, 0.20)"} p={"6"} spaceX={"11"} fontSize={30}>
           <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Acceuil</Link>
-          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Qui suis-je</Link>
+          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Compétences</Link>
           <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Projets</Link>
           <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Contact</Link>
         </HStack>
