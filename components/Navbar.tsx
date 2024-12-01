955b639212578766c22dd9c3247cf3af45ecb8dd
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-01 17:58:28 +0100

    ajout id

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,7 +3,7 @@ import { HStack, Link } from "@chakra-ui/react"
 function Navbar() {
   return (
     <header>
-      <nav>
+      <nav id="home">
         <HStack backgroundColor={"rgba(0, 0, 0, 0.20)"} p={"6"} spaceX={"11"} fontSize={30}>
           <Link href={"#home"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Acceuil</Link>
           <Link href={"#tech"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Compétences</Link>
