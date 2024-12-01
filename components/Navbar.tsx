4c3c6c443f37f0c6df654c6810696f6b30612e2a
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-01 14:15:00 +0100

    ajout redirection nouvelle page

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,8 @@ function Navbar() {
     <header>
       <nav>
         <HStack backgroundColor={"rgba(0, 0, 0, 0.20)"} p={"6"} spaceX={"11"} fontSize={30}>
-          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Acceuil</Link>
-          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Compétences</Link>
+          <Link href={"#home"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Acceuil</Link>
+          <Link href={"#tech"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Compétences</Link>
           <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Projets</Link>
           <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Contact</Link>
         </HStack>
