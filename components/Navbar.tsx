633fcb566073a281198b636d16d3b05be957d616
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-01 14:32:04 +0100

    Ajout href

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,8 @@ function Navbar() {
         <HStack backgroundColor={"rgba(0, 0, 0, 0.20)"} p={"6"} spaceX={"11"} fontSize={30}>
           <Link href={"#home"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Acceuil</Link>
           <Link href={"#tech"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Compétences</Link>
-          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Projets</Link>
-          <Link href={"/"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Contact</Link>
+          <Link href={"#projects"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Projets</Link>
+          <Link href={"#contact"} color={"white"} transition={"all 0.2s"} _hover={{ textDecoration: "underline", scale: 1.1 }}>Contact</Link>
         </HStack>
       </nav>
     </header>
