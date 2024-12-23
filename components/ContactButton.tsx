07caefdc5617b5e6fd9165bd0b254e97085a8e47
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-23 16:55:44 +0100

    Creation du composant contactButton

---
+++ components/ContactButton.tsx
@@ -0,0 +1,9 @@
+function ContactButton() {
+  return (
+    <div className="contact-button text-[40px] font-light mt-[121px] text-center">
+        <a href="https://youtube.com" className="border-4 p-4 block w-[420px]">Contacter</a>
+    </div>
+  )
+}
+
+export default ContactButton
