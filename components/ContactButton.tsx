0cc5a6637468642b67a2ff0fa8f8ab5ca5770c93
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-23 17:01:01 +0100

    Retrait ContactButton, inutile

--- components/ContactButton.tsx
+++
@@ -1,9 +0,0 @@
-function ContactButton() {
-  return (
-    <div className="contact-button text-[40px] font-light mt-[121px] text-center">
-        <a href="https://youtube.com" className="border-4 p-4 block w-[420px]">Contacter</a>
-    </div>
-  )
-}
-
-export default ContactButton
--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -1,12 +1,12 @@
-import ContactButton from "../ContactButton"
-
 function Main() {
     return (
         <section>
             <div className="main-container w-[100vw] h-[100vh] text-white flex justify-center items-center flex-col">
                 <h1 className="name-title font-extrabold text-[80px]">Rayan Ereteo</h1>
                 <p className="subtext font-medium text-[40px]">Un développeur web full stack</p>
-                <ContactButton></ContactButton>
+                <div className="contact-button text-[40px] font-light mt-[121px] text-center">
+                    <a href="https://youtube.com" className="border-4 p-4 block w-[420px]">Contacter</a>
+                </div>
             </div>
         </section>
     )
