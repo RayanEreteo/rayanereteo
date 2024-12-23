5146dd0fd35293e2f4c078d29701e975d02ea472
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-23 21:02:46 +0100

    Ajout des SM

--- components/sections/Main.tsx
+++ components/sections/Main.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image"
+
 function Main() {
     return (
         <section>
@@ -6,6 +8,10 @@ function Main() {
                 <p className="subtext font-medium text-[40px]">Un développeur web full stack</p>
                 <div className="contact-button text-[40px] font-light mt-[121px] text-center transition-all hover:bg-white hover:text-black">
                     <a href="https://youtube.com" className="border-4 p-4 block w-[420px]">Contacter</a>
+                </div>
+                <div className="social-media-container mt-[121px] w-[600px] flex flex-row justify-between">
+                    <a href="https://github.com/RayanEreteo" target="_blank"><Image className="svg-white " src={"/github.svg"} alt="github" width={"100"} height={"100"}></Image></a>
+                    <a href="https://www.linkedin.com/in/rayan-ereteo-6998b9254/" target="_blank"><Image className="svg-white " src={"/linkedin.svg"} alt="github" width={"100"} height={"100"}></Image></a>
                 </div>
             </div>
         </section>
