c96220e3bed0de1fac3e3ec983d35b810c763727
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2024-12-23 14:25:05 +0100

    Création du composant de l'acceuil

---
+++ components/Main.tsx
@@ -0,0 +1,11 @@
+function Main() {
+    return (
+        <section>
+            <div className="main-container w-[100vw] h-[100vh] text-white">
+                <h1 className="name-title font-extrabold">Rayan Ereteo</h1>
+            </div>
+        </section>
+    )
+}
+
+export default Main
