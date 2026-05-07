13700fa3b75d2cd0d9cc56022c2c3b4b27998af3
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-07 18:42:48 +0200

    Ajout navbar

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,35 @@
+function Navbar() {
+    let version = new URLSearchParams(document.location.search).get("ver")
+
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+                    </div>
+                    <ul
+                        tabIndex={-1}
+                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                        <li><a>{version === "classic" ? "Soft Skills" : "Compétences"}</a></li>
+                        <li><a>{version === "classic" ? "Expériences" : "Projets"}</a></li>
+                        <li><a>Me contacter</a></li>
+                    </ul>
+                </div>
+                <a className="btn btn-ghost text-xl">Rayan Ereteo</a>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>{version === "classic" ? "Soft Skills" : "Compétences"}</a></li>
+                    <li><a>{version === "classic" ? "Expériences" : "Projets"}</a></li>
+                    <li><a>Me contacter</a></li>
+                </ul>
+            </div>
+            <div className="navbar-end">
+                <a className="btn btn-primary">Télécharger CV</a>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
