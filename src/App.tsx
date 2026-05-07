4b652b35a9e7e3ae00ddf11ecf4d4c5964b4102c
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-07 18:44:38 +0200

    Ajout navbar et vérification de la version
    Vérification de la version pour un rendu dynamique

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
+  let version = new URLSearchParams(document.location.search).get("ver")
+  if(!version || version === "" || (version != "classic" && version != "dev")) window.location.href = "http://localhost:5173/?ver=classic"
+
   return (
-    <div>
-    </div>
+    <Navbar />
   )
 }
 
