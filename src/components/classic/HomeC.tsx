be32513af28f72da741c8d01118fe003496433d0
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-08 17:35:11 +0200

    changement

--- src/components/classic/HomeC.tsx
+++ src/components/classic/HomeC.tsx
@@ -1,20 +1,22 @@
 
 function HomeC() {
+  const d = new Date();
+  let year = d.getFullYear();
+
   return (
     <section className="" id="HomeC">
-      <div className="hero bg-base-200 min-h-screen">
+      <div className="motion-opacity-in-0 motion-translate-y-in-100 hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
           <img
             src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
             className="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
-            <h1 className="text-5xl font-bold">Box Office News!</h1>
+            <h1 className="text-5xl font-bold">Bienvenue!</h1>
             <p className="py-6">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-              quasi. In deleniti eaque aut repudiandae et a id nisi.
+              Mon nom est Rayan, agé de {Number(year.toString().substring(2, 4)) - 3} ans, je suis un professionnel opérationnel et organisé, dévoué et ambitieux.
             </p>
-            <button className="btn btn-primary">Get Started</button>
+            <a className="btn btn-primary" href="#contact">Me Contacter</a>
           </div>
         </div>
       </div>
