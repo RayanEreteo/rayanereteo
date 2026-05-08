bcd04da684efe015f3084a143b6d77860bee1c00
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-08 13:41:01 +0200

    Changement mineur

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import Navbar from './components/Navbar'
 import VersionSwitcher from './VersionSwitcher'
 
 function App() {
-  const version = new URLSearchParams(document.location.search).get("ver") ?? "classic"
+  const version = new URLSearchParams(document.location.search).get("ver")
   if (version !== "classic" && version !== "dev") window.location.href = "http://localhost:5173/?ver=classic"
 
   return (
     <>
       <Navbar />
-      <VersionSwitcher version={version} />
+      <VersionSwitcher version={version!} />
     </>
   )
 }
