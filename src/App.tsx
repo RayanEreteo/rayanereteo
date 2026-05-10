5d6bc0c3320e14fbe9493be3d525ea3b6c83a645
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-10 16:42:25 +0200

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,7 @@ import VersionSwitcher from './VersionSwitcher'
 
 function App() {
   const version = new URLSearchParams(document.location.search).get("ver")
-  //https://rayanereteo.vercel.app/?ver=classic
-  if (version !== "classic" && version !== "dev") window.location.href = "http://localhost:5173/?ver=classic"
+  if (version !== "classic" && version !== "dev") window.location.href = "https://rayanereteo.vercel.app/?ver=classic"
 
   return (
     <>
