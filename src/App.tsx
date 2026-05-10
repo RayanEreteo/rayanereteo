b75e2e0b6d61e4416d2e804e05f85df09a4ad8e9
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-10 13:09:47 +0200

    Ajout photo de profil

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ import VersionSwitcher from './VersionSwitcher'
 
 function App() {
   const version = new URLSearchParams(document.location.search).get("ver")
-  if (version !== "classic" && version !== "dev") window.location.href = "https://rayanereteo.vercel.app/?ver=classic"
+  //https://rayanereteo.vercel.app/?ver=classic
+  if (version !== "classic" && version !== "dev") window.location.href = "http://localhost:5173/?ver=classic"
 
   return (
     <>
--- src/components/classic/HomeC.tsx
+++ src/components/classic/HomeC.tsx
@@ -8,7 +8,7 @@ function HomeC() {
       <div className="motion-opacity-in-0 motion-translate-y-in-100 hero bg-base-200 min-h-screen">
         <div className="hero-content flex-col lg:flex-row">
           <img
-            src="https://img.daisyui.com/images/stock/photo-1635805737707-575885ab0820.webp"
+            src="src\assets\hidden.webp"
             className="max-w-sm rounded-lg shadow-2xl"
           />
           <div>
