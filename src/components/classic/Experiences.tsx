7db706bc59545b370016de6f359fd5e5b4f63fd2
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-11 16:50:11 +0200

    Ajout experience Super K

--- src/components/classic/Experiences.tsx
+++ src/components/classic/Experiences.tsx
@@ -7,13 +7,13 @@ function Experiences() {
                 <li className="p-4 pb-2 text-xs opacity-60 tracking-wide">Mes expériences professionnelle</li>
 
                 <li className="list-row">
-                    <div><img className="size-20 rounded-box" src="\carrefour.png" /></div>
+                    <div><img className="size-20 rounded-box" src="\superk.png" /></div>
                     <div>
-                        <div>Employé polyvalent - Carrefour</div>
+                        <div>Employé polyvalent - Super K</div>
                         <div className="text-xs uppercase font-semibold opacity-60">CDD</div>
                     </div>
                     <p className="list-col-wrap text-xs">
-                        Ce poste m'as appris la mise en rayon rapide et organisée, tout en me rendant autonome
+                        Ce poste m'as appris la mise en rayon rapide et organisée, tout en me rendant autonome.
                     </p>
                 </li>
 
@@ -26,12 +26,6 @@ function Experiences() {
                     <p className="list-col-wrap text-xs">
                         Ce poste m'as appris a gérer mon stress dans un environment dynamique.
                     </p>
-                    <button className="btn btn-square btn-ghost">
-                        <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M6 3L20 12 6 21 6 3z"></path></g></svg>
-                    </button>
-                    <button className="btn btn-square btn-ghost">
-                        <svg className="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><g strokeLinejoin="round" strokeLinecap="round" strokeWidth="2" fill="none" stroke="currentColor"><path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path></g></svg>
-                    </button>
                 </li>
             </ul>
         </section>
