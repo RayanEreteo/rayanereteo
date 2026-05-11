13e99f7d9f81bd58d402bcc386204a74615c1c67
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-11 16:57:18 +0200

    Ajout phrase de présentation

--- src/components/dev/Skills.tsx
+++ src/components/dev/Skills.tsx
@@ -2,6 +2,7 @@ function Skills() {
   return (
     <section id="skills">
         <h2 className="section-title">Compétences</h2>
+        <p>Ici, vous trouverez tout mon stack. Allant des langages aux frameworks.</p>
     </section>
   )
 }
