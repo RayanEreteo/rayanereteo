eebd505a120572e2ae8784e35537e8b5e64aadf2
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-11 13:33:45 +0200

    Changement mineur

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -18,7 +18,7 @@ function Navbar() {
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl" href="#HomeC">Rayan Ereteo</a>
+                <a className="btn btn-ghost text-xl" href="#Home">Rayan Ereteo</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
