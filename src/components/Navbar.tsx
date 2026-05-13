6951b72feac53a412964fced41cb1d4cea1abad7
Ereteo Rayan <90957589+RayanEreteo@users.noreply.github.com>
2026-05-13 15:21:27 +0200

    Changement href

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
                     <ul
                         tabIndex={-1}
                         className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-                        <li><a href="#softskills">{version === "classic" ? "Soft Skills & Experiences" : "Compétences & Projets"}</a></li>
+                        <li><a href="#skills">{version === "classic" ? "Soft Skills & Experiences" : "Compétences & Projets"}</a></li>
                         <li><a href="#contact">Contact</a></li>
                     </ul>
                 </div>
@@ -22,7 +22,7 @@ function Navbar() {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                    <li><a href="#softskills">{version === "classic" ? "Soft Skills & Experiences" : "Compétences & Projets"}</a></li>
+                    <li><a href="#skills">{version === "classic" ? "Soft Skills & Experiences" : "Compétences & Projets"}</a></li>
                     <li><a href="#contact">Contact</a></li>
                 </ul>
             </div>
